0c18c42c96d3fa15fc38641dff39d512b4c665ec
David Johnson <dhjohn0@gmail.com>
2017-08-17 20:05:50 -0500

    Add authorize function to Controllers

--- controllers/_controller.js
+++ controllers/_controller.js
@@ -13,14 +13,16 @@ module.exports = class Controller {
     methods.forEach((method) => {
       _.forOwn(this[method], (route, key) => {
         if (Array.isArray(route))
-          this.router[method](this.prefix + key, ...route);
+          this.router[method](this.prefix + key, this.authorize, ...route);
         else
-          this.router[method](this.prefix + key, route);
+          this.router[method](this.prefix + key, this.authorize, route);
       });
     });
   }
 
   get prefix() { return '' }
+
+  authorize(req, res, next, user) { return next(); }
 
   get get() { return {}; }
   get post() { return {}; }
