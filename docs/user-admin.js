10c91ffe31de523f21a11c51a08396302d07ad39
David Johnson <dhjohn0@gmail.com>
2017-08-19 10:50:14 -0500

    Add admin role to user-admin doc

--- docs/user-admin.js
+++ docs/user-admin.js
@@ -2,5 +2,8 @@ module.exports = {
   _id: 'e36268c2-77e7-11e7-b151-a30ece72b02b',
   email: 'admin@devadmin.com',
   password: '$2a$10$NnXCbfk6CLiK7fUCPe2snOa4Nhf263eWT7mf4EV5EUmA1x0rsjA7a',
+  roles: {
+    admin: {}
+  },
   type: 'user'
 };
