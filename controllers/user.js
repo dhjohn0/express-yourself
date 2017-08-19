4b5126ab49a33542e8d3a20c333711fdc6914dce
David Johnson <dhjohn0@gmail.com>
2017-08-19 07:18:04 -0500

    Use async/await for user controller

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ let RestfulController = require('./_restful-controller');
 module.exports = class UserController extends RestfulController {
   get prefix() { return '/user'; }
 
-  list(req, res, db) {
+  async list(req, res, db) {
     let start = parseInt(req.query.start) || 0;
     let length = parseInt(req.query.length) || 10;
     let sort = {
@@ -16,104 +16,90 @@ module.exports = class UserController extends RestfulController {
     };
 
     if (req.headers && req.headers['accept'] && req.headers['accept'].toLowerCase().indexOf('application/json') >= 0) {
-      return db.paginate('users/forList', start, length, {
+      let p = await db.paginate('users/forList', start, length, {
         start_key: [sort.column, !sort.desc ? null : {}],
         end_key: [sort.column, !sort.desc ? {} : null],
         descending: sort.desc
-      }).then((p) => {
-        return res.json({
-          list: p.list,
-          pagination: p.pagination,
-          sort: sort
-        });
-      }).catch(e => {
-        console.error(e);
       });
-    }else{
-      return res.render('user/list', {
-        query: {
-          start,
-          length,
-          sort: sort.column,
-          desc: sort.desc
-        }
+
+      return res.json({
+        list: p.list,
+        pagination: p.pagination,
+        sort: sort
       });
     }
+
+    return res.render('user/list', {
+      query: {
+        start,
+        length,
+        sort: sort.column,
+        desc: sort.desc
+      }
+    });
   }
 
   add(req, res) {
     res.render('user/add');
   }
 
-  create(req, res, db, _) {
+  async create(req, res, db, _) {
     let user = _.merge(req.body, {
       enabled: true,
       type: 'user'
     });
 
-    return db.query('users/byEmail', { 
+    let users = await db.query('users/byEmail', { 
       key: user.email,
       include_docs: true 
-    }).then(_users => {
-      let users = _users.rows.map(r => r.doc);
-      if (users.length) {
-        req.flash('error', 'Given email is already in use by another account')
-      }else{
-        user.password = bcrypt.hashSync(user.password);
-        user._id = uuid();
+    })
 
-        return db.put(user).then(() => {
-          req.flash('success', 'User created');
-        });
-      }
-    }).then(() => {
-      res.redirect('/user/add');
-    });
+    if (users.rows.length) {
+      req.flash('error', 'Given email is already in use by another account')
+    }else{
+      user.password = bcrypt.hashSync(user.password);
+      user._id = uuid();
+
+      await db.put(user);
+      
+      req.flash('success', 'User created');
+    }
+    res.redirect('/user/add');
   }
 
-  action(req, res, db) {
+  async action(req, res, db, _) {
     let ids = Array.isArray(req.body._id) || !req.body._id ? req.body._id : [ req.body._id ];
     
-    Promise.resolve().then(() => {
-      if (ids && ids.length) {
-        return db.allDocs({
-          keys: ids, 
-          include_docs: true
-        }).then(result => {
-          let docs = result.rows.map(row => {
-            switch (req.body.action) {
-              case "Delete":
-                row.doc._deleted = true;
-                break;
-              case "Enable":
-                row.doc.enabled = true;
-                break;
-              case "Disable":
-                row.doc.enabled = false;
-                break;
-            }
-            return row.doc;
-          });
-          switch (req.body.action) {
-            case "Delete":
-              req.flash('success', 'Users deleted');
-              break;
-            case "Enable":
-              req.flash('success', 'Users enabled');
-              break;
-            case "Disable":
-              req.flash('success', 'Users disabled');
-              break;
-          }
-          return db.bulkDocs(docs);
-        })
-      }else{
-        req.flash('error', 'No users selected');
+    if (ids && ids.length) {
+      let result = await db.allDocs({
+        keys: ids, 
+        include_docs: true
+      });
+
+      let change = {};
+      switch (req.body.action) {
+        case "Delete":
+          change._deleted = true;
+          break;
+        case "Enable":
+          change.enabled = true;
+          break;
+        case "Disable":
+          change.enabled = false;
+          break;
       }
+
+      let docs = result.rows.map(row => {
+        row.doc = _.extend(row.doc, change);
+        return row.doc;
+      });
       
-  }).then(() => {
+      req.flash('success', `Users ${req.body.action}d`);
+      await db.bulkDocs(docs);
+    }else{
+      req.flash('error', 'No users selected');
+    }
+      
     res.redirect('/user/list');
-  });
-    
   }
 };
