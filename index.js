c98802081681075864adec3d14517b60a1fcfb99
David Johnson <dhjohn0@gmail.com>
2017-08-25 18:11:40 -0500

    Use dependency injection to load app pieces

--- index.js
+++ index.js
@@ -3,52 +3,56 @@ let fs = require('fs');
 let _ = require('lodash');
 let PouchDB = require('pouchdb');
 
-module.exports = (dirpath, config) => {
-  config = require('./lib/config')(dirpath, config);
+module.exports = (appDirectory, config) => {
+  //Setup injection
+  let di = require('./lib/di')();
+  di.value('appDirectory', appDirectory);
+  di.value('config', config);
+  di.value('di', di);
+
+  config = di.invoke(require('./lib/config'));
+  di.value('config', config);
   
   //Setup logging
-  let log = require('./lib/logging')(config);
+  let log = di.invoke(require('./lib/logging'));
+  di.value('log', log);
 
   //Setup handlebars
-  let handlebars = require('./lib/handlebars')(config, log);
+  let handlebars = di.invoke(require('./lib/handlebars'));
+  di.value('handlebars', handlebars);
 
   //Setup db connection
   let db = new PouchDB(config.db);
-  require('./lib/helpers/paginate')(db, log);
-
+  di.value('db', db);
+  di.invoke(require('./lib/helpers/paginate'));
 
   //Setup express
-  let app = require('./lib/express')(config, log);
+  let app = di.invoke(require('./lib/express'));
+  di.value('app', app);
   app.log = log;
+  app.di = di;
 
   //Setup passport
-  let passport = require('./lib/passport')(app, db, log);
-  app.passport = passport;
-
-  //Setup injection
-  let di = require('./lib/di')(app, log);
-  di.value('_', _);
-  di.value('config', config);
-  di.value('db', db);
-  di.value('handlebars', handlebars);
+  let passport = di.invoke(require('./lib/passport'));
   di.value('passport', passport);
-
+  app.passport = passport;
+  
+  di.value('_', _);
   di.factory('user', function (req) {
     return req.user;
   });
-  di.value('log', log);
 
   //Setup Mailer
-  let mailer = require('./lib/mailer')(config, db, handlebars, log);
+  let mailer = di.invoke(require('./lib/mailer'));
   di.value('mailer', mailer);
 
   //Setup controllers
-  require('./lib/controllers')(app, log, config);
+  di.invoke(require('./lib/controllers'));
 
   //mailer.send('dhjohn0@gmail.com', 'Test', '<strong>HELLO!</strong>');
 
   //Setup docs
-  return require('./lib/docs')(app, db, log, config).then(() => {
+  return di.invoke(require('./lib/docs')).then(() => {
     return app;
   });
 }
