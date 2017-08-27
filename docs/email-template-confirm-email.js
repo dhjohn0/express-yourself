6298495815a7190a7edb3451aaeb9543a2440568
David Johnson <dhjohn0@gmail.com>
2017-08-27 17:49:25 -0500

    Add default email confirmed template

---
+++ docs/email-template-confirm-email.js
@@ -0,0 +1,7 @@
+module.exports = {
+  _id: '1df2d7d2-8b67-11e7-9dbc-8ff678e135b0',
+  name: 'confirm-email',
+  subject: 'Confirm Email',
+  body: '<a href="{{{confirmLink}}}">Click here to confirm your email address</a>',
+  type: 'email-template'
+};
